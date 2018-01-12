796e3b5b04e44f562d491c32dcffac9dadd09fdb
keiranevers <32854021+keiranevers@users.noreply.github.com>
2018-01-12 16:06:52 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,99 +1,89 @@
 $(document).ready(function(){
 
-function initializeGame() {
 
 //set variables to play game
-
+	var questionCounter = 0;
+	var correctCount = 0;
+	var incorrectCount = 0;
+	var score = 0;
 	var startGame;
-	var clock 0;
+	var clock = 10;
 	var questionArray = 
 	[
-	{
+		{
 		question: 'What is the piece of plastic covering at the ends of your shoelace, so you don’t have to moisten them with spit to thread them through your shoelace holes?',
 		answerArray: ["Langyard","Aglet","Eyelet","Puggle","Sangler"],
 		correctAnswer: 'Aglet',
 		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/shoelace.jpg'>"
-	},
-	{
+		},
+		{
 		question: 'What are the posts in the parking lot that lets wheelchairs and shopping carts go through, but not your car?',
 		answerArray: ["Bollard","Lenker","Dike","Channel Bar","Liripipe"],
 		correctAnswer: 'Bollard',
 		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/steelpost.jpg'>"
-	},
-	{
+		},
+		{
 		question: 'What are the non-alphanumeric, non-punctuation characters, usually used when you want to write something that you don’t want your children to read?',
 		answerArray: ["Wing-dings","Tarnmark","Dingbat","Binger","Gudgeon"],
 		correctAnswer: 'Dingbat',
 		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/junkchars.jpg'>"
-	},
-	{
+		},
+		{
 		question: 'What is the metal band that connects the pencil eraser to the end of the pencil?',
 		answerArray: ["Nesh","Crottle","Tiffin","Ferrule","Hoggin"],
 		correctAnswer: 'Ferrule',
 		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/eraserthing.jpg'>",
-	},
-	{
+		},
+		{
 		question: 'What is the leather loop in your belt or watch strap that keeps the end in place after it has been fastened through the buckle?',
 		answerArray: ["Keeper","Shofar","Cupre","Probang","Wittol"],
 		correctAnswer: 'Keeper',
 		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/bucklethingy.jpg'>"
-	},
-	{
+		},
+		{
 		question: 'What is the groove made by a sawblade, ie, the width of a cut?',
 		answerArray: ["Dwal","Tiffin","Marlet","Dragomar","Kerf"],
 		correctAnswer: 'Kerf',
 		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/cutseam.jpg'>"
-	},
-	{
+		},
+		{
 		question: 'What is the small indentation at the bottom of a wine bottle, designed to give the bottle extra strength (so they say....), and also, to make it look like it has more wine than it really does (yeah, I hate that.....)?',
 		answerArray: ["Winebibber","Thrutch","Punt","Deceiver","Mim"],
 		correctAnswer: 'Punt',
-		image: 	,"<img id='image' class='rounded mx-auto d-block' src='assets/images/bottlebottom.jpg'>"
-	},
-	{
+		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/bottlebottom.jpg'>"
+		},
+		{
 		question: 'What is the vertical groove between your lip and nose that separates your left and right mustache, unless you’re Hitler, then it’s the part that your mustache covers?',
 		answerArray: ["Sternutator","Philtrum","Mallam","Dariole","Fankle"],
 		correctAnswer: 'Philtrum',
 		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/noselipgroove.jpg'>"
-	},
-	{
+		},
+		{
 		question: 'What are the points of light that you see behind your eyelids when you shut your eyes really hard?',
 		answerArray: ["Famulus","Flocculents","Seraks","Starbuff","Phosphenes"],
 		correctAnswer: 'Phosphenes',
 		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/eyestars.jpg'>"
-	},
-	{
+		},
+		{
 		question: 'What is that little piece of cartilage that sticks out at the front side of your ear?',
 		answerArray: ["Tragus","Shallop","Cerulean","Spurr","Ensorcell"],
 		correctAnswer: 'Tragus',
-		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/earthingy.jpg'>"]
+		image: "<img id='image' class='rounded mx-auto d-block' src='assets/images/earthingy.jpg'>"
 
-	}];
+		}
+	];
 
-	var selectedAnswer;
-	var correctCount = 0;
-	var incorrectCount = 0;
-	var score = 0;
-
-}
-
-initializeGame();
-
-//start the game when click start button - start the clock
-
-$('#start').on('click', startGame), function(event) {
-	setInterval(clock, 100*1000); //setting clock interval 100 seconds (avg 10 seconds per question)
-	generateQuestion();
+function startGame() {
+	$("#start").on("click", generateQuestion);
 
 }
 
-//after selection of answer, go to next question, answer and image
+function generateQuestion() {
+		startGame();
+		$(".clock").html(clock);
+		$("#question").append("<p> question</p>");
+		$("#answer").html(answerArray);
+		$("#image").html(image);
+	}
+});
 
-$('#question'), function(event) {
-	nextQuestion(); //next question, answer and image
-}
-
-//
-
-
-}
